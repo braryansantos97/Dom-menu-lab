1590a7a7842f56e1f786efe78515cbf2f0374625
Brayan Santos <santos.a.brayan97@gmail.com>
2021-05-08 14:41:32 -0400

    part 1 of the lab

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//task 1.0
+
+const mainEl = document.querySelector('main');
+
+//task 1.1
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+//task 1.2
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+
+//task 1.3
+
+mainEl.classList.add('flex-ctr');
+
+//task 2.0
